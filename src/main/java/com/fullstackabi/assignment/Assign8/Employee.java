d82f569b296a117035cd4b3a858a9af2e8388756
BABINAY1 <babinay1+ford@ford.com>
2025-08-19 11:53:58 +0530

    Adding 1 exer from Assign 8

---
+++ src/main/java/com/fullstackabi/assignment/Assign8/Employee.java
@@ -0,0 +1,43 @@
+package com.fullstackabi.assignment.Assign8;
+
+import java.util.Objects;
+
+public class Employee {
+
+    int id;
+    String name;
+    String department;
+    double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+    // TO display the employee with data. otherwise its not printing the employee content.
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
